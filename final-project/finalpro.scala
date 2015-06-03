f39bf57f37a1b334e5bcee74c6298b80feb916d9
dangyang <wy36101299@gmail.com>
2015-06-03 10:31:08 +0800

    add final project scala

--- final-project/finalpro.scala
+++ final-project/finalpro.scala
@@ -63,8 +63,83 @@ def dSameVertexEdges(Vertices:Iterable[String],edgeArray:Array[(String, String)]
 
 val dvertexEdges = dVerCluster.map(x => (x._2,dSameVertexEdges(x._2, edgeArray)))
 
+def numVertex(slength:Int): List[Int] = {
+  var numVertexList = List[Int]()
+  for( i <- 0 to slength-1) {
+    numVertexList :+= i
+  }
+  numVertexList
+}
 
+def newNumEdges(edges:List[(String, String)],zipVertices:List[(Int, String)]): List[(Int, Int)] = {
+  var newEdges = List[(Int, Int)]()
+  var mapEdge = Array(0,0)
+  for ( edge <- edges){ 
+    for ( zipVertex <- zipVertices){  
+      if (zipVertex._2 == edge._1){ mapEdge(0) = zipVertex._1} 
+      if (zipVertex._2 == edge._2){ mapEdge(1) =  zipVertex._1} 
+      }
+    newEdges :+= (mapEdge(0),mapEdge(1))
+    mapEdge = Array(0,0)
+    }
+  newEdges
+}
 
+// cartesian
+implicit class Crossable[X](xs: Traversable[X]) {
+  def cross[Y](ys: Traversable[Y]) = for { x <- xs; y <- ys } yield (x, y)
+}
 
+def tsimilarity(arg1: Iterable[String], arg2:List[(String, String)]): Array[Array[Double]] = {
+  val slength = arg1.toArray.length
+  var matrix = Array.ofDim[Double](slength,slength)
+
+  for (i <- 0 to slength-1){ for ( j <- 0 to slength-1){ if (i == j){matrix(i)(j)=1.0} } }
+
+  var broadcastMatrix = sc.broadcast(matrix)
+
+  val numVertexList = numVertex(slength)
+
+  val zipVertices = numVertexList.zip(arg1)
+
+  val newNumVertices = zipVertices.map(x => x._1)
+
+  val edgesRdd = sc.parallelize(newNumEdges(arg2,zipVertices))
+
+  val vertices = sc.parallelize(numVertexList)
+
+  val neighbor = edgesRdd.flatMap(x => Array((x._2,x._1),(x._1,x._2))).groupByKey()
+  val arrayNeighbor = neighbor.collect().toArray
+  val broadcastArrayNeighbor = sc.broadcast(arrayNeighbor)
+  
+  val upperTmatrix = vertices.cartesian(vertices).filter{ case (a,b) => a < b }
+  val sNeighbor = upperTmatrix.map(x => broadcastArrayNeighbor.value.filter(y => y._1 ==x._1 || y._1 ==x._2)).map(x => (x(0)._2,x(1)._2))
+  val sNeighborLenProduct = sNeighbor.map(x => x._1.size*x._2.size)
+  val sNeighborPairs = sNeighbor.map( x => x._1 cross x._2)
+
+  for (i <- 1 to 100){
+    val NeighborPairsSimilarity = sNeighborPairs.map(x => x.map(y => broadcastMatrix.value(y._1)(y._2)).sum)
+
+    val similarityScore = NeighborPairsSimilarity.zip(sNeighborLenProduct)
+    val Similarity = similarityScore.map(x => x._1/x._2*0.9)
+    var upadteMatrix = broadcastMatrix.value
+    val SimilarityMatrix = upperTmatrix.zip(Similarity)
+    SimilarityMatrix.collect().map(x => upadteMatrix(x._1._1)(x._1._2)=x._2 )
+    for ( i <- 0 to slength-1){ for ( j <- 0 to slength-1){ if (j>i){ upadteMatrix(j)(i) = upadteMatrix(i)(j)} } }
+    broadcastMatrix = sc.broadcast(upadteMatrix)
+  }
+  broadcastMatrix.value
+}
+
+def dSimilarity(dclusters:Array[(Iterable[String], List[(String, String)])] ): List[Array[Array[Double]]] = {
+  var similarityList = List[Array[Array[Double]]]()
+  for( cluster <- dclusters) {
+    val similarity = tsimilarity(cluster._1,cluster._2)
+    similarityList ::= similarity
+  }
+  similarityList
+}
+
+val similarityList = dSimilarity(dvertexEdges.collect())
 
 
